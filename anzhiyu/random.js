f38c7b757fb8bc2f51d33b51307d1aa1dafdda71
007xiu <2392889869@qq.com>
2023-05-29 15:58:07 +0800

    Site updated: 2023-05-29 15:58:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/43de.html","posts/c44e.html","posts/6a89.html","posts/5ea3.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/c44e.html","posts/5ea3.html","posts/43de.html","posts/6a89.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
